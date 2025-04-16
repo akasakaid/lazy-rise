3d139408725b247605cb5880312a1fe9fccefdb5
Fawwaz Thoerif <fawwazthoerif@gmail.com>
2025-04-16 08:30:29 +0700

    ✨ (config.py): add configuration file for service provider, api key, and token symbol to enable bypassing captcha for claiming tokens on faucet website

---
+++ config.py
@@ -0,0 +1,13 @@
+# service provider to bypass captcha
+# available provider
+# 1.) anticaptcha
+# 2.) twocaptcha
+# 3.) capsolver
+provider = ""
+
+# apikey from provider
+apikey = ""
+
+# symbol of the token you want to claim.
+# you can check in https://faucet.testnet.riselabs.xyz/
+TOKEN = "ETH"
